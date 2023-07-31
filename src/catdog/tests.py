e5e846196db915d3a061d84390a69c37271f5555
Evgen <evgen.py86@gmail.com>
2023-07-31 21:32:39 +0300

    test lesson 31.07

--- src/catdog/tests.py
+++ src/catdog/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.urls import reverse
 from catdog.models import AnimalImage
 
@@ -7,8 +7,12 @@ class TestModelAnimalImage(TestCase):
     pass
 
 
-
 class TestCatDogView(TestCase):
+    fixtures = ['data_for_test.json']
+
+    def setUp(self):
+        self.client = Client()
+        self.assertEqual(AnimalImage.objects.count(), 25)
 
     def test_get(self):
         response = self.client.get(reverse('catdog'))
@@ -23,6 +27,8 @@ class TestCatDogView(TestCase):
         self.assertEqual(response.status_code, 200)
         """проверяет, что при рендеринге страницы использовался указанный шаблон"""
         self.assertTemplateUsed(response, template_name='pet.html')
+        """тестирование на создание сессии"""
+        self.assertEqual(self.client.session['data_for_session']['speicies'], 'cat')
 
     def test_dog_post(self):
         response = self.client.post(reverse('catdog'), {'dog': True})
@@ -32,5 +38,8 @@ class TestCatDogView(TestCase):
         self.assertTemplateUsed(response, template_name='pet.html')
 
     def test_reise(self):
-        with self.assertRaisesMessage(ValueError, "invalid literal for int()"):
-            int("a")
+        with self.assertRaisesMessage(AttributeError, ' at home'):
+            self.client.post(reverse('catdog'), {'gg': True})
+
+    def test_save_catdog(self):
+        self.client.post(reverse('catdog'), {'cat': True})
